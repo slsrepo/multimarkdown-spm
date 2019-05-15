08ec29229754e1c4e4d0f58eab59b68089f01d28
Sl <admin@slsrepo.com>
2019-05-15 03:44:12 +0300

    UPDATE: Swift 4.2/5.0 Compatibility

--- Package.swift
+++ Package.swift
@@ -1,9 +1,17 @@
+// swift-tools-version:4.2
+// Change swift-tools-version to the Swift version you want to use.
 import PackageDescription
 
 let package = Package(
-    name: "mmd-swift",
-    targets: [
-    	Target(name: "mmd-swift", dependencies: ["libMultiMarkdown"])
+    name: "libMultiMarkdown",
+    products: [
+        .library(name: "libMultiMarkdown", targets: ["libMultiMarkdown"]),
+        .executable(name: "multimarkdown", targets: ["multimarkdown"])
     ],
-    exclude: ["tests", "Tests", "Sources/multimarkdown"]
+    dependencies: [],
+    targets: [
+        .target(name: "libMultiMarkdown", dependencies: [], path: "./Sources/libMultiMarkdown"),
+        .target(name: "mmd-swift", dependencies: ["libMultiMarkdown"], path: "./Sources/mmd-swift"),
+        .target(name: "multimarkdown", dependencies: ["libMultiMarkdown"], path: "./Sources/multimarkdown")
+    ]
 )
