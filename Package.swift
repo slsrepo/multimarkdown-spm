1b4fb34c3d1fcd9451a28a027c7f14da62dec892
Sl (Shahaf Levi) <admin@slsrepo.com>
2021-02-15 23:58:47 +0200

    ADDED: Swift Package Manager Support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.2
+// Change swift-tools-version to the Swift version you want to use.
+import PackageDescription
+
+let package = Package(
+    name: "multimarkdown-spm",
+    products: [
+        .library(name: "libMultiMarkdown", targets: ["libMultiMarkdown"]),
+        .library(name: "MultiMarkdownSPM", targets: ["MultiMarkdownSPM"]),
+        .executable(name: "multimarkdown", targets: ["multimarkdown"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "libMultiMarkdown", dependencies: [], path: "swift/libMultiMarkdown"),
+        .target(name: "mmd-swift", dependencies: ["libMultiMarkdown"], path: "swift/mmd-swift"),
+        .target(name: "multimarkdown", dependencies: ["libMultiMarkdown"], path: "swift/multimarkdown"),
+        .target(name: "MultiMarkdownSPM", dependencies: ["libMultiMarkdown"], path: "swift/MultiMarkdownSPM")
+    ]
+)
